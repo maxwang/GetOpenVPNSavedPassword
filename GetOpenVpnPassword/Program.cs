da07aa8c3ef6da23d9ed2151b305692e4d134dbe
Max <max@adaxonline.com>
2022-04-12 22:46:21 +1000

    init

---
+++ GetOpenVpnPassword/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Win32;
+
+namespace GetOpenVpnPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configPath = @"Software\OpenVPN-GUI\configs";
+            using (var configs = Registry.CurrentUser.OpenSubKey(configPath))
+            {
+                if (configs != null)
+                {
+                    foreach (var subKey in configs.GetSubKeyNames())
+                    {
+                        Console.WriteLine($"Try to get password for {subKey}");
+                        GetConfigurationPassword(configs, subKey);
+                    }
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void GetConfigurationPassword(RegistryKey? registryKey, string subKey)
+        {
+            using RegistryKey key = registryKey?.OpenSubKey(subKey);
+            var entropy = (byte[])key.GetValue("entropy");
+            var data = (byte[])key.GetValue("key-data");
+
+            if (entropy == null || data == null)
+            {
+                Console.WriteLine("Could not read the value from registry");
+            }
+
+            GetOpenVpnPassword(entropy.Take(entropy.Length - 1).ToArray(), data);
+        }
+
+        private static void GetOpenVpnPassword(byte[] entropy, byte[] data)
+        {
+            var originData = ProtectedData.Unprotect(data, entropy, DataProtectionScope.CurrentUser);
+            var outputString = Encoding.Unicode.GetString(originData);
+            Console.WriteLine(outputString);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Get OpenVPN saved Password
+
+### Development
+
+- Visual Studio 2019
+- .Net Core 3.1
+
+### Note
+
+- You need run this program on the OpenVPN client machine
+- You should have the registry read permission 
